de28690bf09d24d20fe2394348e1dfb21e9d6901
myshin0512@gmail.com <myshin0512@gmail.com>
2024-07-12 00:33:39 +0900

    font_update_1

--- _site/assets/js/search-data.js
+++
@@ -1,6 +0,0 @@
-var store = [{
-        "title": "UnUnlearning: Unlearning is not sufficient for content regulation in advanced generative AI",
-        "excerpt":"Main image  ","categories": ["Paper Review"],
-        "tags": ["unlearning"],
-        "url": "http://localhost:4000/paper%20review/paper-review-ununlearning/"
-      }]
--- index.md
+++
@@ -1,6 +0,0 @@
----
-layout: home
-limit: 16
-show_excerpts: true
-entries_layout: grid
----

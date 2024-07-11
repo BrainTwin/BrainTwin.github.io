56c2fd3dc992765b89ea13ed59db3ed338b3acfe
myshin0512@gmail.com <myshin0512@gmail.com>
2024-07-11 18:17:10 +0900

    test post up

--- _site/assets/js/search-data.js
+++ _site/assets/js/search-data.js
@@ -1,6 +1,6 @@
 var store = [{
-        "title": "Why we study artificial intelligence?",
-        "excerpt":"This is the post content. Archive-index pages should display ","categories": ["tutorials"],
-        "tags": ["post"],
-        "url": "http://localhost:4000/tutorials/basic-form/"
+        "title": "UnUnlearning: Unlearning is not sufficient for content regulation in advanced generative AI",
+        "excerpt":"Main image  ","categories": ["Paper Review"],
+        "tags": ["unlearning"],
+        "url": "http://localhost:4000/paper%20review/paper-review-ununlearning/"
       }]
